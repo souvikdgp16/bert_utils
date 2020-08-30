8059531034a7aa82348d1ea6e68c28b884104d74
Souvik Das <41448784+souvikdgp16@users.noreply.github.com>
2020-08-29 20:02:59 -0400

    Update bert_classifer.py

--- bert_classifer.py
+++ bert_classifer.py
@@ -130,18 +130,18 @@ def main():
 
     args = parser.parse_args()
 
-    try:
-        MAX_LEN = args.max_length
-        bs = args.batch_size
-        epochs = args.epochs
-        model_name_save=args.model_name_save
-        data_file=args.data_file
-        num_classes=args.num_classes
-        test_size=args.test_size
-        model_name=args.model_name
-    except Exception as e:
-        print(e)
-        print("ERROR in parsing tokens!")
+    # try:
+    MAX_LEN = args.max_length
+    bs = args.batch_size
+    epochs = args.epochs
+    model_name_save=args.model_name_save
+    data_file=args.data_file
+    num_classes=args.num_classes
+    test_size=args.test_size
+    model_name=args.model_name
+    # except Exception as e:
+    #     print(e)
+    #     print("ERROR in parsing tokens!")
 
     data=pd.read_csv(data_file)
     X= data["text"].to_list()
